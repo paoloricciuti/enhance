c09cbb36a672d1f14530db4707ded4163fd27890
paoloricciuti <ricciutipaolo@gmail.com>
2023-10-29 23:32:11 +0100

    initial commit

---
+++ src/slides/100/index.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+	import type { Props } from '@slides/types';
+	import enhance from '.it /enhance.gif';
+
+	export const props: Props = {
+		image: enhance,
+	};
+</script>
+
+<p class="font-bold text-6xl">Enhance!</p>
